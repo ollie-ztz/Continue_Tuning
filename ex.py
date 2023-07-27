615b541323844ccbf684188eb287ce0840aafdba
ollie-ztz <tiezhengz@gmail.com>
2023-07-26 22:14:30 -0400

    Update

--- ex.py
+++ ex.py
@@ -4,14 +4,14 @@
 import os
 
 path = '/mnt/tzhang85/AbdomenAtlas_8K_internal'
-files = os.listdir(path)
-files = sorted([f for f in files if f.startswith('08_AbdomenCT-1K')])
+files = sorted(os.listdir(path))[:200]
+# files = sorted([f for f in files if f.startswith('08_AbdomenCT-1K')])
 lines = []
 for f in files:
-    line = os.path.join(f, 'ct.nii.gz') + '\t' + os.path.join(f,'pseudo_label.nii.gz')
+    line = os.path.join(f, 'ct.nii.gz') + '\t' + os.path.join(f,'segmentations')
     lines.append(line)
 out = './dataset/dataset_list'
-save_file = 'PAOT_08_CL.txt'
+save_file = 'chongyusb.txt'
 with open(os.path.join(out, save_file), 'w') as f:
     f.write('\n'.join(lines))
     
