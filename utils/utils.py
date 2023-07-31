e6784e34fab4fac9502a898c603ab6b1c6cc5159
ollie-ztz <tiezhengz@gmail.com>
2023-07-31 16:11:54 -0400

    Update

--- utils/utils.py
+++ utils/utils.py
@@ -61,7 +61,7 @@ TEMPLATE={
     'all': [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32], 
     'target':[1,2,3,4,6,7,8,9,11], ## target organ index
     'assemble':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
-    'finetune':[4,7,8,9],
+    'finetune':[1,2,3,6,11],
 }
 
 ORGAN_NAME = ['Spleen', 'Right Kidney', 'Left Kidney', 'Gall Bladder', 'Esophagus', 
