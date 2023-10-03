cf9f50cd64989121c7af107c1bd77047bd436d04
ollie-ztz <tiezhengz@gmail.com>
2023-10-03 12:47:46 -0400

    Update

--- ex.py
+++ ex.py
@@ -1,17 +1,8 @@
-# read the PAOT_08.txt file 
-
-
+import numpy as np
+import nibabel as nib
 import os
-
-path = '/mnt/tzhang85/AbdomenAtlas_8K_internal'
-files = sorted(os.listdir(path))[:200]
-# files = sorted([f for f in files if f.startswith('08_AbdomenCT-1K')])
-lines = []
-for f in files:
-    line = os.path.join(f, 'ct.nii.gz') + '\t' + os.path.join(f,'segmentations')
-    lines.append(line)
-out = './dataset/dataset_list'
-save_file = 'chongyusb.txt'
-with open(os.path.join(out, save_file), 'w') as f:
-    f.write('\n'.join(lines))
     
+path = '/mnt/zzhou82/PublicAbdominalData/12_CT-ORG/label/labels-0.nii.gz'
+img = nib.load(path)
+data = img.get_fdata()
+print(np.where(data==4))
