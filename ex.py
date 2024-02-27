f93fabc10d793ed7c3dcaa918e905ec6c2632097
ollie-ztz <tiezhengz@gmail.com>
2024-02-27 13:20:40 -0500

    update

--- ex.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import nibabel as nib
-import os
-    
-path = '/mnt/zzhou82/PublicAbdominalData/12_CT-ORG/label/labels-0.nii.gz'
-img = nib.load(path)
-data = img.get_fdata()
-print(np.where(data==4))
